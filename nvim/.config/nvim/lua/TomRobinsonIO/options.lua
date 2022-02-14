4af670168acf39ee4227cbaab6ab9deaf25d9b0d
TomRobinsonIO <Tom@TomRobinson.io>
2022-02-14 14:13:01 -0500

    gruvbox-material transparent bg

--- nvim/.config/nvim/lua/TomRobinsonIO/options.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/options.lua
@@ -21,9 +21,10 @@ o.incsearch = true                -- While typing a search command, show where t
 o.wrap = false                    -- Display lines as one long line
 
 g["gruvbox_material_palette"] = "mix"
+g["gruvbox_material_transparent_background"] = 1
 
-c "colorscheme gruvbox-material"  -- This is the way
-c "highlight Normal guibg=none"   -- Transparent background
 c "set path+=**"                  -- Set path to include subfolders
 c "set nohlsearch"                -- Turn of highlighting when search is done
 c "set guicursor="                -- Don't change cursor when entering insert mode
+c "colorscheme gruvbox-material"  -- This is the way
+c "highlight Normal guibg=none"   -- Transparent background
