b64e58ecd3383b83d51507b8a36e2b2fc4bade92
TomRobinsonIO <Tom@TomRobinson.io>
2022-08-16 14:54:56 -0400

    Rounded separators

--- nvim/.config/nvim/lua/TomRobinsonIO/lualine.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/lualine.lua
@@ -83,8 +83,8 @@ require ('lualine').setup {
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "toggleterm" },
     always_divide_middle = true,
   },
