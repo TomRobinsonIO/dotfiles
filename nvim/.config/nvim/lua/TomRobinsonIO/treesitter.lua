20fbb9fb1da414edd1ba5916e52f0011ecf123c9
TomRobinsonIO <Tom@TomRobinson.io>
2022-10-06 13:28:50 -0400

    'quick fixes for updates'

--- nvim/.config/nvim/lua/TomRobinsonIO/treesitter.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/treesitter.lua
@@ -5,40 +5,13 @@ end
 
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
 	highlight = {
-    use_languagetree = true,
 		enable = true, -- false will disable the whole extension
-		-- disable = { "css", "html" }, -- list of language that will be disabled
 		disable = { "css" }, -- list of language that will be disabled
-		additional_vim_regex_highlighting = true,
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "yaml", "css" } },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-	autotag = {
-		enable = true,
-		disable = { "xml" },
-	},
-	rainbow = {
-		enable = false,
-		colors = {
-			"Gold",
-			"Orchid",
-			"DodgerBlue",
-			"Cornsilk",
-			"Salmon",
-			"LawnGreen",
-		},
-		disable = { "html" },
-	},
-	playground = {
-		enable = true,
-	},
+	indent = { enable = true, disable = { "python", "css" } },
 })
