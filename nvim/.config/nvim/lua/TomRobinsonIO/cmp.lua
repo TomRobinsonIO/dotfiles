e4f3a5102fcb9a4ac7536e49a558a671f6970e22
TomRobinsonIO <Tom@TomRobinson.io>
2023-03-12 13:58:41 -0400

    added custom snippets from sbulav

--- nvim/.config/nvim/lua/TomRobinsonIO/cmp.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/cmp.lua
@@ -11,6 +11,7 @@ if not snip_status_ok then
 end
 
 require("luasnip/loaders/from_vscode").lazy_load()
+require("TomRobinsonIO.snippets")
 
 local check_backspace = function()
   local col = vim.fn.col "." - 1
