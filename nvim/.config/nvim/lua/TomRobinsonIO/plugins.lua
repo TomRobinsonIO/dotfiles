33235bc2f0032295225ecc5483796c93ef7d3409
TomRobinsonIO <Tom@TomRobinson.io>
2023-07-25 17:39:40 -0400

    picky catppuccin

--- nvim/.config/nvim/lua/TomRobinsonIO/plugins.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/plugins.lua
@@ -64,7 +64,7 @@ return packer.startup(function(use)
     use "joshdick/onedark.vim"
     use "Mofiqul/dracula.nvim"
     use "shaunsingh/nord.nvim"
-    use "catppuccin/nvim"
+    use {"catppuccin/nvim", as = "catppuccin" }
 
     -- cmp plugins
     use "hrsh7th/nvim-cmp" -- The completion plugin
