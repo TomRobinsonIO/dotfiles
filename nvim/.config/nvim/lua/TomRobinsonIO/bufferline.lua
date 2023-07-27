bf26009c3a996911914d1d63486bb0a099767103
TomRobinsonIO <tom@tomrobinson.io>
2023-07-27 14:20:32 -0400

    options

--- nvim/.config/nvim/lua/TomRobinsonIO/bufferline.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/bufferline.lua
@@ -3,6 +3,12 @@ if not status_ok then
     return
 end
 
-bufferline.setup{
+local icons = require "TomRobinsonIO.icons"
 
+bufferline.setup{
+    options = {
+        indicator = { icon = icons.ui.LineLeft, style = "icon" },
+        offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+        truncate_names = true,
+    },
 }
