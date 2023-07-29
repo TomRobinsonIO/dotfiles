b29419ba921fcad0c3ca6c52113a84841ecb8a14
TomRobinsonIO <Tom@TomRobinson.io>
2023-07-29 15:55:13 -0400

    nord cause bored

--- nvim/.config/nvim/lua/TomRobinsonIO/options.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/options.lua
@@ -28,7 +28,8 @@ g["gruvbox_material_palette"] = "mix"
 g["gruvbox_material_transparent_background"] = 1
 g.catppuccin_flavour = "mocha"
 
-c "colorscheme catppuccin"        -- gruvbox_material, dracula, catppuccin, nord
+-- c "colorscheme catppuccin"        -- gruvbox_material, dracula, catppuccin, nord
+c "colorscheme nord"        -- gruvbox_material, dracula, catppuccin, nord
 c "hi colorcolumn guibg=#89b4fa"  -- Set the colorcolumn color to catppuccin blue
 c "set guicursor="                -- Don't change cursor when entering insert mode
 c "set nohlsearch"                -- Turn of highlighting when search is done
