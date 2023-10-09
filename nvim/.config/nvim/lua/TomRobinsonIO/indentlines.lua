b2783a8276aed3c091a00be72c9d1e6d7a253d43
TomRobinsonIO <Tom@TomRobinson.io>
2023-10-09 14:26:42 -0400

    Update config for 3.0

--- nvim/.config/nvim/lua/TomRobinsonIO/indentlines.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/indentlines.lua
@@ -1,28 +1,66 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
   return
 end
 
 local icons = require "TomRobinsonIO.icons"
 
-indent_blankline.setup{
-    enabled = true,
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = {
-    "help",
-    "startify",
-    "dashboard",
-    "lazy",
-    "neogitstatus",
-    "NvimTree",
-    "Trouble",
-    "text",
-    },
-    char = icons.ui.LineLeft,
-    context_char = icons.ui.LineLeft,
-    show_trailing_blankline_indent = false,
-    show_first_indent_level = true,
-    use_treesitter = true,
-    show_current_context = true,
-    show_current_context_start = true
+local highlight = {
+    "nord15",
+    "nord14",
+    "nord13",
+    "nord12",
+    "nord11",
 }
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "nord7", { fg = "#8fbcbb" })
+    vim.api.nvim_set_hl(0, "nord8", { fg = "#88c0d0" })
+    vim.api.nvim_set_hl(0, "nord9", { fg = "#81a1c1" })
+    vim.api.nvim_set_hl(0, "nord10", { fg = "#5e81ac" })
+    vim.api.nvim_set_hl(0, "nord11", { fg = "#bf616a" })
+    vim.api.nvim_set_hl(0, "nord12", { fg = "#d08770" })
+    vim.api.nvim_set_hl(0, "nord13", { fg = "#ebcb8b" })
+    vim.api.nvim_set_hl(0, "nord14", { fg = "#a3be8c" })
+    vim.api.nvim_set_hl(0, "nord15", { fg = "#b48ead" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+
+ibl.setup{
+    enabled = true,
+    exclude  = {
+        buftypes = {
+            "terminal",
+            "nofile",
+        },
+        filetypes = {
+            "help",
+            "startify",
+            "dashboard",
+            "lazy",
+            "neogitstatus",
+            "NvimTree",
+            "Trouble",
+            "text",
+        },
+    },
+    indent = {
+        char = {icons.ui.LineLeft},
+        -- highlight = highlight,
+    },
+    scope = {
+        enabled = true,
+        show_start = true,
+        highlight = highlight,
+        include = {
+            node_type = {
+                ["*"] = { "*" }
+            },
+        },
+    },
+}
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
