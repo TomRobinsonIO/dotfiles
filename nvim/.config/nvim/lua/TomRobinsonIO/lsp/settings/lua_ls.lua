c234b62c5c7aa2f499e096b0c7ac51465c918d29
TomRobinsonIO <github@mail.tomrobinson.io>
2024-12-31 12:10:47 -0500

    Add 3rd party luv library

--- nvim/.config/nvim/lua/TomRobinsonIO/lsp/settings/lua_ls.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/lsp/settings/lua_ls.lua
@@ -6,8 +6,9 @@ return {
       },
       workspace = {
         library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.stdpath "config" .. "/lua"] = true,
+          vim.fn.expand "$VIMRUNTIME/lua",
+          vim.fn.stdpath "config" .. "/lua",
+          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy", "${3rd}/luv/library",
         },
       },
     },
