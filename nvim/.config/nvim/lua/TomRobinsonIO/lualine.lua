bbad493e47079ecd91315cab24619495d9539ee5
TomRobinsonIO <github@mail.tomrobinson.io>
2024-12-31 12:11:47 -0500

    Replace depreciated nvim_buff_get_option w/ nvim_get_option_value

--- nvim/.config/nvim/lua/TomRobinsonIO/lualine.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/lualine.lua
@@ -66,7 +66,7 @@ local progress = function()
 end
 
 local spaces = function()
-  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+  return "spaces: " .. vim.api.nvim_get_option_value("shiftwidth", { buf = 0 })
 end
 
 local nvim_navic = function()
