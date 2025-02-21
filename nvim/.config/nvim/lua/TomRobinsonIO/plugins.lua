a456b920cafeb0c405aa1ac6d7eee43d61bc81db
TomRobinsonIO <Github@mail.TomRobinson.io>
2025-02-21 09:56:05 -0500

    Add render-markdown.nvim, formatting

--- nvim/.config/nvim/lua/TomRobinsonIO/plugins.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/plugins.lua
@@ -1,72 +1,72 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
 local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
-  return
+    return
 end
 
 -- Install plugins here
 local plugins = {
-    "folke/lazy.nvim", -- Have lazy manage itself
-    "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-    "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+    "folke/lazy.nvim",        -- Have lazy manage itself
+    "nvim-lua/popup.nvim",    -- An implementation of the Popup API from vim in Neovim
+    "nvim-lua/plenary.nvim",  -- Useful lua functions used ny lots of plugins
     "nvim-lualine/lualine.nvim", -- Status Line for Neovim
     "nvim-tree/nvim-web-devicons",
     "nvim-tree/nvim-tree.lua", -- File Explorer for Neovim
-    "airblade/vim-rooter", -- Root to CWD
+    "airblade/vim-rooter",    -- Root to CWD
     "nvim-telescope/telescope.nvim",
     "nvim-telescope/telescope-fzy-native.nvim",
-    "kylechui/nvim-surround", -- Add/Edit surrounding pairs
-    "tpope/vim-fugitive", -- Vim Git itigrations
-    "tpope/vim-rhubarb", -- Vim Git browser
-    "tpope/vim-commentary", -- Comment lines with gcc
-    "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
-    "rcarriga/nvim-notify", -- A fancy, configurable, notification manager for Neovim
-    "folke/todo-comments.nvim", -- H ighlight and search for todo comments like TODO, HACK, BUG in your code base
-    "SmiteshP/nvim-navic", -- show your current code context
+    "kylechui/nvim-surround",           -- Add/Edit surrounding pairs
+    "tpope/vim-fugitive",               -- Vim Git itigrations
+    "tpope/vim-rhubarb",                -- Vim Git browser
+    "tpope/vim-commentary",             -- Comment lines with gcc
+    "windwp/nvim-autopairs",            -- Autopairs, integrates with both cmp and treesitter
+    "rcarriga/nvim-notify",             -- A fancy, configurable, notification manager for Neovim
+    "folke/todo-comments.nvim",         -- H ighlight and search for todo comments like TODO, HACK, BUG in your code base
+    "SmiteshP/nvim-navic",              -- show your current code context
     "lukas-reineke/indent-blankline.nvim", -- Indentation guides
-    "akinsho/bufferline.nvim", -- Snazzy Bufferline
+    "akinsho/bufferline.nvim",          -- Snazzy Bufferline
 
     -- Colorschemes
     "sainnhe/gruvbox-material",
     "joshdick/onedark.vim",
     "Mofiqul/dracula.nvim",
     "shaunsingh/nord.nvim",
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "catppuccin/nvim",    name = "catppuccin",  priority = 1000 },
 
     -- Completion plugins
-    "hrsh7th/nvim-cmp", -- The completion plugin
-    "hrsh7th/cmp-buffer", -- buffer completions
-    "hrsh7th/cmp-path", -- path completions
-    "hrsh7th/cmp-cmdline", -- cmdline completions
+    "hrsh7th/nvim-cmp",      -- The completion plugin
+    "hrsh7th/cmp-buffer",    -- buffer completions
+    "hrsh7th/cmp-path",      -- path completions
+    "hrsh7th/cmp-cmdline",   -- cmdline completions
     "saadparwaiz1/cmp_luasnip", -- snippet completions
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-emoji",
     "hrsh7th/cmp-nvim-lua",
 
     -- Snippets
-    "L3MON4D3/LuaSnip", --snippet engine
+    "L3MON4D3/LuaSnip",          --snippet engine
     "rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
     -- LSP
-    "neovim/nvim-lspconfig", -- enable LSP
-    "williamboman/mason.nvim", -- simple to language server installer
+    "neovim/nvim-lspconfig",        -- enable LSP
+    "williamboman/mason.nvim",      -- simple to language server installer
     "williamboman/mason-lspconfig.nvim",
     "tamago324/nlsp-settings.nvim", -- language server settings defined in json for
     "jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
@@ -74,11 +74,12 @@ local plugins = {
     "simrat39/symbols-outline.nvim",
     "ray-x/lsp_signature.nvim",
     "b0o/SchemaStore.nvim",
-    { "folke/trouble.nvim", cmd = "TroubleToggle", },
+    { "folke/trouble.nvim", cmd = "TroubleToggle" },
     "RRethy/vim-illuminate",
+    "MeanderingProgrammer/render-markdown.nvim", -- Render Markdown Syntax in Buffer
 
     -- Treesitter
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     "JoosepAlviste/nvim-ts-context-commentstring",
     "nvim-treesitter/playground",
     "windwp/nvim-ts-autotag",
