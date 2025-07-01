5fba86e3488eb474f61b928745f143e1ea880e9f
TomRobinsonIO <Github@mail.TomRobinson.io>
2025-07-01 08:28:21 -0400

    Formatting

--- nvim/.config/nvim/lua/TomRobinsonIO/lsp/handlers.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/lsp/handlers.lua
@@ -42,11 +42,11 @@ M.setup = function()
     vim.diagnostic.config(config)
 
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
+        border = "rounded",
     })
 
     vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
+        border = "rounded",
     })
 end
 
