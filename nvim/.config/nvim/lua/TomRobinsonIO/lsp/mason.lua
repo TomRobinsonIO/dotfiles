22cdbb7fc757d1d45246b398e8b96aa30eb6f634
TomRobinsonIO <Github@mail.TomRobinson.io>
2025-11-01 13:36:48 -0400

    Changed deprecated lspconfig to vim.lsp.config and vim.lsp.enable

--- nvim/.config/nvim/lua/TomRobinsonIO/lsp/mason.lua
+++ nvim/.config/nvim/lua/TomRobinsonIO/lsp/mason.lua
@@ -1,53 +1,50 @@
 local servers = {
-	"lua_ls",
-	"cssls",
-	"html",
+    "lua_ls",
+    "cssls",
+    "html",
     "marksman",
-	"pyright",
-	"bashls",
-	"jsonls",
-	"yamlls",
+    "pyright",
+    "bashls",
+    "jsonls",
+    "yamlls",
 }
 
 local settings = {
-	ui = {
-		border = "none",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
+    ui = {
+        border = "none",
+        icons = {
+            package_installed = "◍",
+            package_pending = "◍",
+            package_uninstalled = "◍",
+        },
+    },
+    log_level = vim.log.levels.INFO,
+    max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-	ensure_installed = servers,
-	automatic_installation = true,
+    ensure_installed = servers,
+    automatic_installation = true,
 })
-
-local lspconfig_status_ok, lspconfig = pcall(vim.lsp.config)
-if not lspconfig_status_ok then
-    print('mason broke')
-	return
-end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-	opts = {
-		on_attach = require("TomRobinsonIO.lsp.handlers").on_attach,
-		capabilities = require("TomRobinsonIO.lsp.handlers").capabilities,
-	}
+    opts = {
+        on_attach    = require("TomRobinsonIO.lsp.handlers").on_attach,
+        capabilities = require("TomRobinsonIO.lsp.handlers").capabilities,
+    }
 
-	server = vim.split(server, "@")[1]
+    server = vim.split(server, "@")[1]
 
-	local require_ok, conf_opts = pcall(require, "TomRobinsonIO.lsp.settings." .. server)
-	if require_ok then
-		opts = vim.tbl_deep_extend("force", conf_opts, opts)
-	end
+    local require_ok, conf_opts =
+        pcall(require, "TomRobinsonIO.lsp.settings." .. server)
+    if require_ok then
+        opts = vim.tbl_deep_extend("force", conf_opts, opts)
+    end
 
-	lspconfig[server].setup(opts)
+    vim.lsp.config(server, opts)
+
+    vim.lsp.enable(server)
 end
